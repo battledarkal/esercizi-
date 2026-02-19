069aa701c8ad24ee249b1b351b8bef5c09deb22f
battledarkal <steamjacopo31@gmail.com>
2026-02-19 22:17:29 +0100

    fine ? consecutivi.c

---
+++ consecutivi.c
@@ -0,0 +1,29 @@
+//L’utente deve continuare a inserire dei caratteri fino a che non inserisce uno dopo l’altro due caratteri consecutivi (ad esempio si ferma dopo aver letto uno dopo l’altro ‘a’ e ‘b’).
+
+#include <stdio.h>
+
+int main () {
+
+char primo, secondo, extra;
+int corrente = 0, precedente = 0;
+
+printf("inserisci il primo carattere: ");
+scanf("%c", &primo);
+
+printf("inserisci il secondo carattere: ");
+scanf("%c", &secondo;
+
+primo = precedente
+secondo = corrente
+
+while(corrente = precedente + 1) {
+precedente = corrente
+printf("inserisci un nuovo carattere");
+scanf("%c", &extra);
+
+extra = corrente
+
+    }
+
+printf("Hai inserito due caratteri consecutivi");
+}
