afef49805203547718b0b9e694e6e1c8734c0185
battledarkal <steamjacopo31@gmail.com>
2026-04-22 10:33:04 +0200

    Update 99a0.c

--- 99a0.c
+++ 99a0.c
@@ -7,9 +7,13 @@ int array[100], i;
 
 for(int i = 99; i > -1; i--) {
     
-array[i] = i;
-printf("%d \n", array[i]);
+array[i] = 99 - i;
+
+}
+
+for(int i = 0; i < 100; i++) {
     
+printf("%d\n", array[i]);    
 }
 
 return 0;
