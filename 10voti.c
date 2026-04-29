d66cef2a9989d5dc6198e36301b55e00d20ee4e3
battledarkal <steamjacopo31@gmail.com>
2026-04-29 10:16:35 +0200

    Update 10voti.c

--- 10voti.c
+++ 10voti.c
@@ -6,27 +6,16 @@ int main() {
 
 	for(int i = 0; i < 10; i++) {
 
-		printf("inserisci uno dei dieci ultimi voti:  ");
+		printf("inserisci il voto %d: ", i + 1);
 		scanf("%d", &array[i]);
-
-		if(array[i] == 6) {
-			voti[0]++;
-		} else {
-			if(array[i] == 7) {
-				voti[1]++;
-			} else {
-				if(array[i] == 8) {
-					voti[2]++;
-				} else {
-					if(array[i] == 9) {
-						voti[3]++;
-					} else {
-						if(array[i] == 10) voti[4]++;
-					}
-				}
-			}
-		}
 	}
+
+	for(int i = 0; i < 10; i++) {
+		voti[array[i]-6]++;
+	}
+
+
 	printf("hai preso 6 %d volte, 7 %d volte, 8 %d volte, 9 %d volte e 10 %d volte", voti[0],voti[1],voti[2],voti[3],voti[4]);
 	return 0;
 }
+
